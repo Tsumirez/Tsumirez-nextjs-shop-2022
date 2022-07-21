0a1cb8ef125622f79f8f38f4c518f10ee28fa7fc
zefirez <tsumirez@zoho.com>
2022-07-21 17:41:41 +0200

    Added app state management with useContext hook.
    It's in StateContext component. It's functionalities have been hooked
    to product details page ([slug].js) and Navbar component (for shopping cart display).
    And it wraps the whole app in _app.js file.

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,10 +3,13 @@ import { urlFor, client } from '../../lib/client';
 
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index,setIndex] =  useState(0);
+  const { decQty, incQty, qty, onAdd } = useStateContext();
+
   return (
     <div>
         <div className='product-detail-container'>
@@ -43,15 +46,15 @@ const ProductDetails = ({ product, products }) => {
                 <div className='quantity'>
                     <h3>Quantity:</h3>
                     <p className='quantity-desc'>
-                        <span className='minus' onClick=""><AiOutlineMinus /></span>
-                        <span className='num' onClick="">0</span>
-                        <span className='minus' onClick=""><AiOutlinePlus /></span>
+                        <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+                        <span className='num' onClick="">{qty}</span>
+                        <span className='minus' onClick={incQty}><AiOutlinePlus /></span>
                     </p>
                 </div>
                 <div className='buttons'>
                     <button type="button" 
                       className='add-to-cart'
-                      onClick="">
+                      onClick={() => {onAdd(product, qty)}}>
                         Add to Cart
                       </button>
 
